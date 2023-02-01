14bb5658bea4b3d75ce3d439fb81d31d98787bad
RomainDanizel <romain.danizel@hetic.net>
2023-02-01 16:55:59 +0100

    page login and session storage still need localstorage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Fragment } from "react";
-import Home from "./component/Home/Home";
-import HomeComponent from "./component/Home/Home";
+import { Fragment, useState } from "react";
+import Dashboard from "./component/Dashboard/Dashboard";
+import Preference from "./component/Preference/Preference";
+import Login from "./component/Login/Login";
+import useToken from "./component/service/useToken";
+
+
 function App() {
+  const {token, setToken} = useToken()
+
+  if (!token) {
+    return <Login setToken= {setToken}/>
+  }
   return (
     <Fragment>
+      <h1>Application</h1>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomeComponent/>}></Route>
+          <Route path="/dashboard" element={<Dashboard/>}></Route>
+          <Route path='/preference' element={<Preference/>}></Route>
         </Routes>
       </BrowserRouter>
     </Fragment>
---
+++ src/component/Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function Navbar() {
+  return (
+    <nav>
+        <link to='/'><img src=""></img></link>
+    </nav>
+  )
+}

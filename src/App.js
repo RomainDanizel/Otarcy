83f8832b829fbe0b109c3bee4c95f59eea3c8491
Mylene <mylene.mzali@edu.devinci.fr>
2023-02-01 14:46:51 +0100

    route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Fragment } from "react";
+import Home from "./component/Home/Home";
+import HomeComponent from "./component/Home/Home";
 function App() {
   return (
-    <div className="App">
-      <p>it work</p>
-    </div>
+    <Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeComponent/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Fragment>
   );
 }
-
 export default App;

8cc6e02ef4cd56d482113b6acb3769959bf7a5cb
David Tessier <david.tessier2@edu.devinci.fr>
2023-02-03 13:47:17 +0100

    Pushing code for the search feature

---
+++ src/App.js
@@ -0,0 +1,51 @@
+function MyButton() {
+    return (
+      <button>
+        I'm a button
+      </button>
+    );
+  }
+
+
+  function FindingAutocomplete(x, array){
+    if(x.length >= 1){
+    let validname = []
+
+    array.forEach(function(name){
+        if(x.substr(0, x.length).toLowerCase() == name.substr(0, x.length)){
+                validname.push(name)  
+        }
+    } )
+    return validname;
+    /*
+    if(validname.length == 1){
+        searchbar.innerHTML= ''
+    }
+    if(typeof validname[2] === 'undefined'){
+        searchbar.innerHTML= '<div class="autocomplete search_width">' + '<p class="autocomplete_value">' +validname[1].charAt(0).toUpperCase() + validname[1].slice(1)+ '</p>' + '</div>'; 
+    } 
+    else if(typeof validname[3] === 'undefined') {
+        searchbar.innerHTML= '<div class="autocomplete search_width">' + '<p class="autocomplete_value">' +validname[1].charAt(0).toUpperCase() + validname[1].slice(1)+ '</p>'+ '</div> <div class="autocomplete search_width">' + '<p class="autocomplete_value">' +validname[2].charAt(0).toUpperCase() + validname[2].slice(1)+ '</p>'+ '</div>';  
+    }
+    else {
+        searchbar.innerHTML= '<div class="autocomplete search_width">' + '<p class="autocomplete_value">' +validname[1].charAt(0).toUpperCase() + validname[1].slice(1)+ '</p>'+ '</div> <div class="autocomplete search_width">' + '<p class="autocomplete_value">' +validname[2].charAt(0).toUpperCase() + validname[2].slice(1)+ '</p>'+ '</div> <div class="autocomplete search_width">' + '<p class="autocomplete_value">' +validname[3].charAt(0).toUpperCase() + validname[3].slice(1)+ '</p>'+ '</div>';  
+    }
+    */
+  }
+  
+} 
+
+let array = ['carrot', 'potato', 'tomato' , 'tata', 'worm', 'cola'];
+
+console.log(FindingAutocomplete('t', array));
+console.log('should have worked')
+
+  export default function MyApp() {
+    return (
+      <div>
+        <h1>Welcome to my app</h1>
+        <MyButton />
+      </div>
+    );
+  }
+  

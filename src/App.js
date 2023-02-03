6852e5baf22fcc72796eaaa55ccf74ffb676a936
Yann Decelle <yann.decelle@edu.devinci.fr>
2023-02-03 12:32:47 +0100

    added legume comp

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, {} from 'react';
+import Legume from './legume';
 
 const App = () => {
-  const [data, setData] = useState({});
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetch("https://openfarm.cc/api/v1/crops/tomato");
-      const json = await result.json();
-      setData(json.data);
-    };
-
-    fetchData();
-  }, []);
 
   return (
     <div>
-      {data.attributes && (
-        <div>
-          <h2>{data.attributes.name}</h2>
-          <img src={data.attributes.main_image_path} alt={data.attributes.name} />
-          <h3>{data.attributes.binomial_name}</h3>
-          <p>{data.attributes.description}</p>
-        </div>
-      )}
+      <Legume/>
     </div>
   );
 };
